f748dd7b2da81fe48361be0d539233976b4715cb
FukushimaTakeshi <whippet.818@gmail.com>
2018-04-16 22:13:55 +0900

    changed to env

--- make_call.rb
+++ make_call.rb
@@ -3,14 +3,14 @@ Bundler.require
 require 'twilio-ruby'
 
 # Get your Account Sid and Auth Token from twilio.com/console
-account_sid = 'ACd47fec33b5186a1a24c522e70e967d21'
-auth_token = 'e6256ee1619e013f77e67fd92efbd83b'
+account_sid = ENV['TWILIO_ACCOUNT_SID']
+auth_token = ENV['TWILIO_AUTH_TOKEN']
 
 # set up a client to talk to the Twilio REST API
 @client = Twilio::REST::Client.new account_sid, auth_token
 
 call = @client.calls.create(
-    to: "+818061400882",
-    from: "+815031963578",
+    to: ENV['TWILIO_MY_PHONE_NUMBER'],
+    from: ENV['TWILIO_PHONE_NUMBER'],
     url: "http://demo.twilio.com/docs/voice.xml")
 puts call.to
